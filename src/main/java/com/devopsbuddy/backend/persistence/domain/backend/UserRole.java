6a538b772980fb55eb85fbe4962341d2f2e9bef3
TIEMPODEV\ramon pvazquez <rpineda@tiempodevelopment.com>
2017-10-10 11:47:43 -0500

    Create the JPA entity model for Spring Security

---
+++ src/main/java/com/devopsbuddy/backend/persistence/domain/backend/UserRole.java
@@ -0,0 +1,73 @@
+package com.devopsbuddy.backend.persistence.domain.backend;
+
+import java.io.Serializable;
+
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="user_role")
+public class UserRole implements Serializable {
+
+	/** The Serial Version UID for Serializable classes. **/
+	private static final long serialVersionUID = 1L;
+
+	public UserRole() {
+	}
+
+	@Id
+	@ManyToOne(fetch=FetchType.EAGER)
+	@JoinColumn(name="user_id")
+	private User user;
+
+	@Id
+	@ManyToOne(fetch=FetchType.EAGER)
+	@JoinColumn(name="role_id")
+	private Role role;
+
+	public User getUser() {
+		return user;
+	}
+
+	public void setUser(User user) {
+		this.user = user;
+	}
+
+	public Role getRole() {
+		return role;
+	}
+
+	public void setRole(Role role) {
+		this.role = role;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((role == null) ? 0 : role.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		UserRole other = (UserRole) obj;
+		if (role == null) {
+			if (other.role != null)
+				return false;
+		} else if (!role.equals(other.role))
+			return false;
+		return true;
+	}
+
+}
