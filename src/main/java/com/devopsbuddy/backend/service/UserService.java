8faf4dcf163d6d0a76a99d4cb6306bb4e429760d
TIEMPODEV\ramon pvazquez <rpineda@tiempodevelopment.com>
2017-10-11 15:10:20 -0500

    Added password encryption to Spring Security

--- src/main/java/com/devopsbuddy/backend/service/UserService.java
+++ src/main/java/com/devopsbuddy/backend/service/UserService.java
@@ -3,6 +3,7 @@ package com.devopsbuddy.backend.service;
 import java.util.Set;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -26,9 +27,15 @@ public class UserService {
 	
 	@Autowired
 	private UserRepository userRepository;
-	
+
+	@Autowired
+	private BCryptPasswordEncoder passwordEncoder;
+
 	@Transactional
 	public User createUser(User user, PlanEnum planEnum, Set<UserRole> userRoles) {
+
+		String encryptedPassword = passwordEncoder.encode(user.getPassword());
+		user.setPassword(encryptedPassword);
 
 		Plan plan = new Plan(planEnum);
 
