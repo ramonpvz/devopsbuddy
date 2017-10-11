7ef90cea2ff19edf31e4e7956caede8a2a6717d1
TIEMPODEV\ramon pvazquez <rpineda@tiempodevelopment.com>
2017-10-11 16:17:58 -0500

    Externalised webmaster login and email details

--- src/main/java/com/devopsbuddy/DevopsbuddyApplication.java
+++ src/main/java/com/devopsbuddy/DevopsbuddyApplication.java
@@ -6,6 +6,7 @@ import java.util.Set;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -20,26 +21,32 @@ import com.devopsbuddy.utils.UserUtils;
 
 @SpringBootApplication
 public class DevopsbuddyApplication implements CommandLineRunner {
-	
+
 	/** The application logger **/
 	private static final Logger LOG = LoggerFactory.getLogger(DevopsbuddyApplication.class);
-	
+
 	@Autowired
 	private UserService userService;
+
+	@Value("${webmaster.username}")
+	private String webmasterUsername;
+
+	@Value("${webmaster.password}")
+	private String webmasterPassword;
+
+	@Value("${webmaster.email}")
+	private String webmasterEmail;
 
 	public static void main(String[] args) {
 		SpringApplication.run(DevopsbuddyApplication.class, args);
 	}
 
 	@Override
-	public void run(String... arg0) throws Exception {		
-
-		String username = "proUser";
-		String email = "proUser@devopsbuddy.com";
-
-		User user = UserUtils.createBasicUser(username, email);
+	public void run(String... arg0) throws Exception {
+		User user = UserUtils.createBasicUser(webmasterUsername, webmasterEmail);
+		user.setPassword(webmasterPassword);
 		Set<UserRole> userRoles = new HashSet<>();
-		userRoles.add(new UserRole(user, new Role(RolesEnum.BASIC)));
+		userRoles.add(new UserRole(user, new Role(RolesEnum.ADMIN)));
 		LOG.debug("Creating user with username {}", user.getUsername());
 		userService.createUser(user, PlanEnum.PRO, userRoles);
 		LOG.info("User {} created", user.getUsername());
