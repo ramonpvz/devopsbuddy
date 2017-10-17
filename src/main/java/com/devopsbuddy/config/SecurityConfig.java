5d35abdb7ceb9221177abf179c0d27886f5a2828
TIEMPODEV\ramon pvazquez <rpineda@tiempodevelopment.com>
2017-10-17 15:27:23 -0500

    Created signup form and handler method

--- src/main/java/com/devopsbuddy/config/SecurityConfig.java
+++ src/main/java/com/devopsbuddy/config/SecurityConfig.java
@@ -16,6 +16,7 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 import com.devopsbuddy.backend.service.UserSecurityService;
 import com.devopsbuddy.web.controllers.ForgotMyPasswordController;
+import com.devopsbuddy.web.controllers.SignupController;
 
 @Configuration
 @EnableWebSecurity
@@ -49,7 +50,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 			"/console/**",
 			ForgotMyPasswordController.FORGOT_PASSWORD_URL_MAPPING,
 			ForgotMyPasswordController.CHANGE_PASSWORD_VIEW_NAME,
-			ForgotMyPasswordController.CHANGE_PASSWORD_PATH
+			SignupController.SIGNUP_URL_MAPPING
 	};
 
 	@Override
