8035cba47fd3e7282d0d5334f85710b3481f21d3
Ramon Pineda Vazquez <nomar@Ramons-MacBook-Pro.local>
2017-10-19 17:22:34 -0500

    S3 - Part 1

--- src/main/java/com/devopsbuddy/config/ApplicationConfig.java
+++ src/main/java/com/devopsbuddy/config/ApplicationConfig.java
@@ -1,10 +1,21 @@
 package com.devopsbuddy.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+import com.amazonaws.auth.AWSCredentials;
+import com.amazonaws.auth.AWSStaticCredentialsProvider;
+import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.auth.profile.ProfileCredentialsProvider;
+import com.amazonaws.regions.Region;
+import com.amazonaws.regions.Regions;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AmazonS3Client;
+import com.amazonaws.services.s3.AmazonS3ClientBuilder;
 
 @Configuration
 @EnableJpaRepositories(basePackages = "com.devopsbuddy.backend.persistence.repositories")
@@ -12,7 +23,26 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 @EnableTransactionManagement
 @PropertySource("file:///${user.home}/.devopsbuddy/application-common.properties")
 public class ApplicationConfig {
+
+	@Value("${aws.s3.profile}")
+	private String awsProfileName;
 	
+	public AmazonS3Client s3ClientDep() {
+		
+		/*AWSCredentials credentials = new ProfileCredentialsProvider(awsProfileName).getCredentials();
+		AmazonS3Client s3Client = new AmazonS3Client(credentials);
+		
+		Region region = Region.getRegion(Regions.EU_WEST_1);
+		s3Client.setRegion(region);*/
+
+		return null; 
+		
+	}
 	
+	public AmazonS3 s3Client() {
+		BasicAWSCredentials creds = new BasicAWSCredentials("access_key", "");
+		AmazonS3 s3Client = AmazonS3ClientBuilder.standard().withCredentials(new AWSStaticCredentialsProvider(creds)).build();
+		return s3Client;
+	}
 
 }
