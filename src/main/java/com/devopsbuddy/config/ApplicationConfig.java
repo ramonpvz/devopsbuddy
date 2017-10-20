ccf1be60c22c9cbb6dd77135d507ddf8db900afe
TIEMPODEV\ramon pvazquez <rpineda@tiempodevelopment.com>
2017-10-20 12:58:14 -0500

    Completed the S3 Service

--- src/main/java/com/devopsbuddy/config/ApplicationConfig.java
+++ src/main/java/com/devopsbuddy/config/ApplicationConfig.java
@@ -2,17 +2,14 @@ package com.devopsbuddy.config;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
-import com.amazonaws.auth.AWSCredentials;
 import com.amazonaws.auth.AWSStaticCredentialsProvider;
 import com.amazonaws.auth.BasicAWSCredentials;
-import com.amazonaws.auth.profile.ProfileCredentialsProvider;
-import com.amazonaws.regions.Region;
-import com.amazonaws.regions.Regions;
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.AmazonS3Client;
 import com.amazonaws.services.s3.AmazonS3ClientBuilder;
@@ -28,19 +25,16 @@ public class ApplicationConfig {
 	private String awsProfileName;
 	
 	public AmazonS3Client s3ClientDep() {
-		
 		/*AWSCredentials credentials = new ProfileCredentialsProvider(awsProfileName).getCredentials();
 		AmazonS3Client s3Client = new AmazonS3Client(credentials);
-		
 		Region region = Region.getRegion(Regions.EU_WEST_1);
 		s3Client.setRegion(region);*/
-
-		return null; 
-		
+		return null;
 	}
-	
+
+	@Bean
 	public AmazonS3 s3Client() {
-		BasicAWSCredentials creds = new BasicAWSCredentials("access_key", "");
+		BasicAWSCredentials creds = new BasicAWSCredentials("AKIAJBOISJFLKMZUALPA","PfErRRkTfwqfaWEzOqKFD/M7k8twnDEmx7GmpPdR");
 		AmazonS3 s3Client = AmazonS3ClientBuilder.standard().withCredentials(new AWSStaticCredentialsProvider(creds)).build();
 		return s3Client;
 	}
